4db61d90437b68c4c28e989fe92b908c84c76e7e
David Robertson <davidjrobertson@live.co.uk>
2017-03-08 16:14:47 +0000

    finished the lab

--- lab.rb
+++ lab.rb
@@ -13,9 +13,8 @@ puts lines[1]
 
 # 3. How many ways can we return 'Princes Street' from the array?
 
+puts lines[4]
 puts lines[-1]
-puts lines[5]
-
 princes_street = lines.pop()
 puts princes_street
 
@@ -37,7 +36,11 @@ puts lines.delete("Edinburgh Park")
 
 # 8. Delete 'Edinburgh Park' from the array by index
 
+puts lines.delete_at(1)
+
 # 9. Reverse the positions of the stops in the array
+
+puts lines.reverse
 
 ### B. Given the following data structure:
 
@@ -45,13 +48,25 @@ my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
 
+puts my_hash[1]
+
 # 2. How would you return the string `"Two"`?
+
+puts my_hash[2]
 
 # 3. How would you return the number `2`?
 
+puts my_hash["two"]
+
 # 4. How would you add `{3 => "Three"}` to the hash?
 
+puts my_hash["3"] = "Three"
+
+
 # 5. How would you add `{:four => 4}` to the hash?
+
+puts my_hash[:four] = 4
+
 
 ### C. Given the following data structure:
 
@@ -89,18 +104,50 @@ users = {
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 
+puts users["Jonathan"] [:twitter]
+
 # 2. Return Erik's hometown
+
+puts users ["Erik"] [:home_town]
 
 # 3. Return the array of Erik's favorite numbers
 
+puts users ["Erik"] [:favourite_numbers]
+
 # 4. Return the type of Avril's pet Colin
+
+puts users ["Avril"] [:pets].values
 
 # 5. Return the smallest of Erik's favorite numbers
 
+puts users ["Erik"] [:favourite_numbers].min
+
 # 6. Add the number `7` to Erik's favorite numbers
+
+puts users ["Erik"] [:favourite_numbers] << [7]
 
 # 7. Change Erik's hometown to Edinburgh
 
+puts users ["Erik"] [:home_town] = ["Edinburgh"]
+
 # 8. Add a pet dog to Erik called "Fluffy"
 
+puts users ["Erik"] [:pets] [:Fluffy] = "dog"
+
 # 9. Add yourself to the users hash
+
+my_hash = {"David" => {
+  :twitter => "djr291",
+  :favourite_numbers => [21],
+  :home_town => "Roslin",
+  :pets => {
+    "cleo" => :cat
+          }
+          }
+          } 
+
+puts my_hash
+
+users = my_hash.merge(users)
+
+puts users
